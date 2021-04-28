2b3e0ee7aecea54552d9f5069befb3ef5dcfe2a1
Riolite <Riolite@outlook.com>
2021-04-28 23:42:21 +0200

    Update structure

--- src/com/riolite/leetcode/Main.java
+++ src/com/riolite/leetcode/Main.java
@@ -1,16 +1,7 @@
 package com.riolite.leetcode;
 
-import com.riolite.leetcode.bsearch.easy.ConvertSortedArrayToBinarySearchTree;
-import com.riolite.leetcode.model.TreeNode;
-
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        TreeNode root = ConvertSortedArrayToBinarySearchTree.sortedArrayToBST(new int[]{-1,0,1,2});
-        System.out.println("nums=> "+ Arrays.toString(new int[]{-10,-3,0,5,9}));
-        TreeNode root1 = ConvertSortedArrayToBinarySearchTree.sortedArrayToBSTOpt(new int[]{-10,-3,0,5,9}, 0, 4);
-        System.out.println();
     }
 }
--- src/com/riolite/leetcode/bsearch/easy/ConvertSortedArrayToBinarySearchTree.java
+++ src/com/riolite/leetcode/bsearch/easy/ConvertSortedArrayToBinarySearchTree.java
@@ -7,33 +7,41 @@ import java.util.Arrays;
 
 public class ConvertSortedArrayToBinarySearchTree {
     public static TreeNode sortedArrayToBST(int[] nums) {
-        if(nums.length < 1){
+        if (nums.length < 1) {
             return null;
         }
-        if(nums.length == 1) {
+        if (nums.length == 1) {
             return new TreeNode(nums[0]);
         }
-        int middleValue = nums[nums.length/2];
-        int[] lSubArray = Arrays.stream(nums).limit(nums.length/2).toArray();
+        int middleValue = nums[nums.length / 2];
+        int[] lSubArray = Arrays.stream(nums).limit(nums.length / 2).toArray();
         TreeNode left = sortedArrayToBST(lSubArray);
-        int[] rSubArray = Arrays.stream(nums).skip((nums.length+2) /2).toArray();
+        int[] rSubArray = Arrays.stream(nums).skip((nums.length + 2) / 2).toArray();
         TreeNode right = sortedArrayToBST(rSubArray);
-        return new TreeNode(middleValue,left,right);
+        return new TreeNode(middleValue, left, right);
     }
 
-    public static TreeNode sortedArrayToBSTOpt(int[] nums, int start, int end) {
+    public static TreeNode sortedArrayToBSTOptimized(int[] nums) {
+        return _sortedArrayBSTOptimized(nums, 0, nums.length - 1);
+    }
+
+    public static TreeNode _sortedArrayBSTOptimized(int[] nums, int start, int end) {
         int length = end - start + 1;
-        int index = start + ((end-start)/2);
-        if(length < 1){
+        if (length < 1) {
             return null;
         }
-        if(length == 1) {
+        int index = start + ((end - start) / 2);
+        if (length == 1) {
             return new TreeNode(nums[index]);
         }
-
         int middleValue = nums[index];
-        TreeNode left = sortedArrayToBSTOpt(nums, start, index-1);
-        TreeNode right = sortedArrayToBSTOpt(nums, index+1, end);
-        return new TreeNode(middleValue,left,right);
+        TreeNode left = _sortedArrayBSTOptimized(nums, start, index - 1);
+        TreeNode right = _sortedArrayBSTOptimized(nums, index + 1, end);
+        return new TreeNode(middleValue, left, right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = sortedArrayToBST(new int[]{-1, 0, 1, 2});
+        root =sortedArrayToBSTOptimized(new int[]{-10, -3, 0, 5, 9});
     }
 }
